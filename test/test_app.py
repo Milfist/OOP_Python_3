c915a6606fdc3cbf687471f22ae27b299965e8cd
MilfisT <milfist@gmail.com>
2018-06-08 10:59:11 +0200

    [FIX] App test fixed.

--- test/test_app.py
+++ test/test_app.py
@@ -5,5 +5,5 @@ import unittest
 class AppTest(unittest.TestCase):
 
     def test_main(self):
-        result = os.system("python ../App.py")
+        result = os.system("python App.py")
         self.assertEqual(result, 0)
