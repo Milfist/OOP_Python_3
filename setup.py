09731fb32cb9155efeb204fbf6ce50334e3beaec
MilfisT <milfist@gmail.com>
2018-06-11 13:45:28 +0200

    [CHORE]  Package distribution configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="OOP_Python_3",
+    version="0.1.0",
+    description="",
+    author="",
+    url="",
+    scripts=[],
+    packages=["inheritance", "models"]
+)
