e191be2b8489f74efa5cc875226169a00d7b69b4
MilfisT <milfist@gmail.com>
2018-06-11 09:01:01 +0200

    [FEAT] Finished models

--- inheritance/bike.py
+++ inheritance/bike.py
@@ -2,5 +2,10 @@ from inheritance.vehicle import Vehicle
 
 
 class Bike(Vehicle):
-    pass
 
+    def __init__(self, color, wheels_number, bike_type):
+        super().__init__(color, wheels_number)
+        self.bike_type = bike_type
+
+    def __str__(self):
+        return super().__str__() + ' {}'.format(self.bike_type)
--- inheritance/motorcycle.py
+++ inheritance/motorcycle.py
@@ -2,4 +2,11 @@ from inheritance.bike import Bike
 
 
 class Motorcycle(Bike):
-    pass
+
+    def __init__(self, color, wheels_number, bike_type, velocity, power):
+        super().__init__(color, wheels_number, bike_type)
+        self.velocity = velocity
+        self.power = power
+
+    def __str__(self):
+        return super().__str__() + ' {} {}'.format(self.velocity, self.power)
